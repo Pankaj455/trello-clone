34583ce1843565745113ffda9d8e6dda8994279e
Pankaj Das <pdas500450@gmail.com>
2023-02-11 16:55:30 +0530

    modified reset link

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -267,7 +267,7 @@ const forgotPassword = async (req, res) => {
     const resetToken = user.generateResetToken();
     await user.save();
 
-    const resetLink = `https://tcrello.netlify.app/user/reset-password/${resetToken}`;
+    const resetLink = `https://ccrello.netlify.app/user/reset-password/${resetToken}`;
 
     const message = `<p>Just click the link to reset your password - <a href='${resetLink}'>${resetLink}</a></p>`;
 
